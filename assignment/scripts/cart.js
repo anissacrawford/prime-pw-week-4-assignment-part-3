3f5dff912c3aa22cdb2370a12867a097f3d2e363
Anissa Crawford <anissacrawford@gmail.com>
2022-02-08 13:18:14 -0600

    created addItem function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,22 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem(item){
+  //console.log('in addItem', item);
+  basket.push(item);
+  return true;
+}
+
+//function listitems(){
+
+//}
+
+//function empty(){
+
+//}
+
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
